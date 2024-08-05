b64ca54a966beeb59f3c464b1067856184ea465d
rshet007@yahoo.com <rshet007@yahoo.com>
2024-08-05 20:26:31 +0530

    modified Multipy.java file with message

--- Multiply.java
+++ Multiply.java
@@ -1,8 +1,8 @@
 class Multiply{
     public static void main(String[] args){
         int a =10;
-int b=7;
-int multiply=a*b;
-System.out.println(multiply);
+        int b=7;
+        int multiply=a*b;
+        System.out.println("Product : "+multiply);
     }
 }
