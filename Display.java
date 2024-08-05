95e00c68659dc2543a29bf2168505abad5ba7c75
rshet007@yahoo.com <rshet007@yahoo.com>
2024-08-05 20:01:50 +0530

    05-08-2024 8pm Added Display.java and Sum.java

---
+++ Display.java
@@ -0,0 +1,7 @@
+package USTGlobalDemo;
+
+class Display{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,13 @@
+package USTGlobalDemo;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        int a,b c;
+        a=100;
+        b=200;
+        c=a+b;
+        System.out.println("Sum : "+c);
+    }
+    
+}
