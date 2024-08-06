c98599da4f83e7dc61f6e60368a704edec4b9f9b
rshet007@yahoo.com <rshet007@yahoo.com>
2024-08-06 20:10:45 +0530

    Modified Area.java and added Helloworld.java

--- Area.java
+++ Area.java
@@ -1,11 +1,17 @@
 
 public class Area {
- public static int rectangle(int a,int b){
+
+    public static int areaOfRectangle(int a,int b){
         return a*b;
     }
-    public static double areacircle(int r)
+
+    public static double areaOfCircle(int r)
     {
         return 3.14*r*r;
+    }
+
+    public static double areaOfTraingle(int d1,int d2){
+        return 0.5*d1*d2;
     }
 }
 
